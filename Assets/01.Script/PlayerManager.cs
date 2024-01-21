d0ff9b535f635f83d52dbc028132ea9185c36bd9
LKM0222WIN <python03@naver.com>
2024-01-21 23:53:14 +0900

    add :PlayerImg, BGDataBase추가, 빵 정보 추가

--- Assets/01.Script/PlayerManager.cs
+++ Assets/01.Script/PlayerManager.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerManager : MonoBehaviour
 {
+    [SerializeField] List<GameObject> TaskList = new List<GameObject>();
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +18,11 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
+
+    #region TaskFunc
+    public void MakeBread(){
+        
+    }
+
+    #endregion
 }
